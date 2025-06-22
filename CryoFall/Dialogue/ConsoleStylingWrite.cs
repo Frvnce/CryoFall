3fc0f5efded02ff91e58625c159c6efaedde7c3f
Costas04 <86519026+Costas04@users.noreply.github.com>
2025-06-22 21:54:16 +0200

    GAMEOVER

--- CryoFall/Dialogue/ConsoleStylingWrite.cs
+++ CryoFall/Dialogue/ConsoleStylingWrite.cs
@@ -317,6 +317,7 @@ public static class ConsoleStylingWrite
                 case "inputName": AskPlayerPlaceHolders("playerName",current.Text); break;
                 case "inputNameAssistente": AskPlayerPlaceHolders("assistant",current.Text); break;
                 case "pullLever": OpenDoor("zona_carburante_nord",rm); break;
+                case "gameOver": GameOver(); break;
                 default: WriteDialogue(current.Character, current.Kind, current.SpeakerName,current.Text, liveWriting: liveWriting); break;
             }
             
@@ -338,6 +339,15 @@ public static class ConsoleStylingWrite
             }
             Thread.Sleep(msToWaitForLine); //aspetta x tempo tra un dialogo e l'altro.
         }
+    }
+
+    private static void GameOver()
+    {   
+        AnsiConsole.WriteLine("  ____    _    __  __ _____    _____     _______ ____  _ \n / ___|  / \\  |  \\/  | ____|  / _ \\ \\   / / ____|  _ \\| |\n| |  _  / _ \\ | |\\/| |  _|   | | | \\ \\ / /|  _| | |_) | |\n| |_| |/ ___ \\| |  | | |___  | |_| |\\ V / | |___|  _ <|_|\n \\____/_/   \\_\\_|  |_|_____|  \\___/  \\_/  |_____|_| \\_(_)");
+        AnsiConsole.WriteLine();
+        AnsiConsole.MarkupLine("[yellow]Premi un tasto per uscire...[/]");
+        Console.ReadKey(intercept: true); // aspetta che il giocatore premi un tasto.
+        Environment.Exit(0);
     }
 
     /// <summary>
